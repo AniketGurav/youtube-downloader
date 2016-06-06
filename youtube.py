65ee8992f03a78004ce68dd8d80d5155535fac71
Will Beddow <will.beddow@gmail.com>
2016-06-06 15:47:43 -0400

    Additions

--- youtube.py
+++ youtube.py
@@ -4,70 +4,30 @@ import urllib
 import urllib2
 import os
 import re
-
-
-#Note, If you are on a windows system, go through and uncomment the lines that say os.chdir("Python27/Scripts")
-#This plugin also depends on the installation of youtube-dl
-
+import sys
+def download(n):
+    if 5 >= n >= 1:
+        if 'win' in sys.platform:
+            os.chdir('/Python27/Scripts')
+        os.system("youtube-dl {0}".format("http://www.youtube.com/watch?v={0}".format(search_results[n - 1])))
+def url_gen(vidurl):
+	youtube = etree.HTML(urllib.urlopen(vidurl).read())
+	return youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
 
 def youtube(vidsearch):
-    query_string = urllib.urlencode({"search_query" : vidsearch})  
-    html_content = urllib2.urlopen("http://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode('utf-8'))
-    vidurl1 = ("http://www.youtube.com/watch?v=" + search_results[0])
-    vidurl2 = ("http://www.youtube.com/watch?v=" + search_results[2])
-    vidurl3 = ("http://www.youtube.com/watch?v=" + search_results[3])
-    vidurl4 = ("http://www.youtube.com/watch?v=" + search_results[4])
-    vidurl5 = ("http://www.youtube.com/watch?v=" + search_results[5])
-    youtube = etree.HTML(urllib.urlopen(vidurl1).read()) #enter your youtube url here
-    video_title1 = youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
-    print('1.'+''.join(video_title1))
-    youtube = etree.HTML(urllib.urlopen(vidurl2).read()) #enter your youtube url here
-    video_title2 = youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
-    print('2.'+''.join(video_title2))
-
-    youtube = etree.HTML(urllib.urlopen(vidurl3).read()) #enter your youtube url here
-    video_title3 = youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
-    print('3.'+''.join(video_title3))
-
-    youtube = etree.HTML(urllib.urlopen(vidurl4).read()) #enter your youtube url here
-    video_title4 = youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
-    print('4.'+''.join(video_title4))
-
-    youtube = etree.HTML(urllib.urlopen(vidurl5).read()) #enter your youtube url here
-    video_title5 = youtube.xpath("//span[@id='eow-title']/@title") #get xpath using firepath firefox addon
-    print('5.'+''.join(video_title5))
-
-    whichvid = raw_input("Which video would you like to perform an action on? (1,2, etc.)")
-    if '1' in whichvid:
-        vidaction = raw_input("Would you like to download this video? (y/n)")
-        if vidaction == 'y': 
-            #os.chdir('/Python27/Scripts')
-            os.system("youtube-dl " + vidurl1)
+	query_string = urllib.urlencode({"search_query" : vidsearch})
+	html_content = urllib2.urlopen("http://www.youtube.com/results?" + query_string)
+	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode('utf-8'))
+	for i in range(0,4):
+		print ("{0}.".format(str(i+1)+''.join(url_gen(search_results("http://www.youtube.com/watch?v={0}".format(search_results[i]))))))
+	whichvid = raw_input("Which video would you like to perform an action on? (1,2, etc.)")
+	try:
+	    n = int(whichvid)
+        download(n)
+    except TypeError:
+        print "Must be valid number between 1 and 5"
+        youtube(vidsearch)
 
 
-    elif '2' in whichvid:
-        vidaction = raw_input("Would you like to download this video? (y/n)")
-        if vidaction == 'y': 
-            #os.chdir('/Python27/Scripts')
-            os.system("youtube-dl " + vidurl2)
-
-    elif '3' in whichvid:
-        vidaction = raw_input("Would you like to download this video? (y/n)")
-        if vidaction == 'y': 
-           # os.chdir('/Python27/Scripts')
-            os.system("youtube-dl " + vidurl3)
-
-    elif '4' in whichvid:
-        vidaction = raw_input("Would you like to download this video? (y/n)")
-        if vidaction == "y": 
-            #os.chdir('/Python27/Scripts')
-            os.system("youtube-dl " + vidurl4)
-
-    elif '5' in whichvid:
-        vidaction = raw_input("Would you like to download this video? (y/n)")
-        if vidaction == 'y': 
-            #os.chdir('/Python27/Scripts')
-            os.system("youtube-dl " + vidurl5)
-q = raw_input('>')
-youtube(q)
+if __name__ == "__main__":
+	youtube(raw_input('>'))
